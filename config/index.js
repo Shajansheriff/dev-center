fde156e94540c8dc6f7a26f1de773abc8a980132
Shajahan <shajansheriff@gmail.com>
2018-08-25 18:12:21 +0530

    add basic routes

---
+++ config/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://devadmin:DevAdmin45@ds133252.mlab.com:33252/dev-center'
+}
--- server.js
+++ server.js
@@ -1,6 +1,22 @@
-const express = require("express");
+const express = require('express');
+const mongoose = require('mongoose');
+
+// load routes
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 const app = express();
+const config = require('./config/index');
+
+mongoose.connect(config.mongoURI)
+        .then(() => console.log('MongoDB connected.'))
+        .catch((error) => console.log(error));
+
+// use routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
